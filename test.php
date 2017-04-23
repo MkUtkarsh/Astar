9e65a10d15e3dba57e9fb87ce2f7d904c76d2bee
ApoorvPandey9524 <apoorv.pandey9524@gmail.com>
2017-04-24 01:38:38 +0530

    index1.php can be used as home page
    user enters the value of rows in matrix
    and matrix builds up.first click turns a box green (startnode) second
    click turns another box red(endnode)
    IDs are assigned in form of "row,column"
    id of start node and endnode is stored in JS variables 'startnode' and
    'endnode' respectively.

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ function heuristic($nodeStart ,$nodeEnd) {
 	print_r($Arr1);
 	print_r($Arr2);
 	$hueristic = sqrt(pow(abs($Arr2[1] - $Arr1[1]),2) + pow(abs($Arr2[0] - $Arr1[0]),2))  ;
-	print_r($hueristic);
+	
 	}
 $nodeStart = "1,1";
 $nodeEnd = "6,13";
