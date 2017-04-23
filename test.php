4eddaa508c07294a13b3fadc363d39bfb98c3f1e
Mkhacktivist <Utkarsh.mk84@gmail.com>
2017-04-23 18:08:23 +0530

    Commit Mk
    not a good code

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+function heuristic($nodeStart ,$nodeEnd) {
+	$Arr1 = explode(',',$nodeStart);
+	$Arr2 = explode(',',$nodeEnd); 
+	print_r($Arr1);
+	print_r($Arr2);
+	$hueristic = sqrt(pow(abs($Arr2[1] - $Arr1[1]),2) + pow(abs($Arr2[0] - $Arr1[0]),2))  ;
+	print_r($hueristic);
+	}
+$nodeStart = "1,1";
+$nodeEnd = "6,13";
+	
+heuristic($nodeStart,$nodeEnd);
