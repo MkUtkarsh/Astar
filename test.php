e3f1f1833dd51be7b26e14120b42f256bede495c
Mkhacktivist <Utkarsh.mk84@gmail.com>
2017-04-23 20:55:45 +0530

    added  function findAdjacentNodes
    added function for finding adjacent nodes

--- test.php
+++ test.php
@@ -1,4 +1,38 @@
 <?php
+
+global $nodeEnd;
+$nodeStart = "0,2";
+$nodeEnd = "6,13";
+
+
+
+findAdjacentNodes($nodeStart);
+
+function findAdjacentNodes($nodeStart) {
+	$adjacentNodes = array();
+	$nodeCoordinates = explode(',',$nodeStart);
+	$nodeCoordinates[0] = $nodeCoordinates[0] + 1; 
+	array_push($adjacentNodes,$nodeCoordinates);
+	$nodeCoordinates[0] = $nodeCoordinates[0] - 2;
+	array_push($adjacentNodes,$nodeCoordinates);
+	$nodeCoordinates[1] = $nodeCoordinates[1] + 1;
+	$nodeCoordinates[0] = $nodeCoordinates[0] + 1;
+	array_push($adjacentNodes,$nodeCoordinates);
+	$nodeCoordinates[1] = $nodeCoordinates[1] - 2;
+	array_push($adjacentNodes,$nodeCoordinates);
+
+//	print_r($nodeStart);
+//	echo '<pre>';
+//	print_r($adjacentNodes);
+	
+	neighbourHeuristic($adjacentNodes,$GLOBALS['nodeEnd']);
+	}
+
+function neighbourHeuristic($adjacentNodes, $nodeEnd) {
+	echo '<pre>';
+	print_r($adjacentNodes);
+	}
+
 function heuristic($nodeStart ,$nodeEnd) {
 	$Arr1 = explode(',',$nodeStart);
 	$Arr2 = explode(',',$nodeEnd); 
@@ -7,7 +41,5 @@ function heuristic($nodeStart ,$nodeEnd) {
 	$hueristic = sqrt(pow(abs($Arr2[1] - $Arr1[1]),2) + pow(abs($Arr2[0] - $Arr1[0]),2))  ;
 	print_r($hueristic);
 	}
-$nodeStart = "1,1";
-$nodeEnd = "6,13";
-	
-heuristic($nodeStart,$nodeEnd);
+
+	//heuristic($nodeStart,$nodeEnd);
